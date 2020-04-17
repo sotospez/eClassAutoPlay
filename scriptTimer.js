4d3e54db5bcccebcc1e463a7c5a5f90287bca275
sotiris <sotospez@gmail.com>
2020-04-17 17:27:16 +0300

    test ver 0.1

--- scriptTimer.js
+++ scriptTimer.js
@@ -2,36 +2,26 @@
  * Auto-play Script for slides viewer on elearningclass.eu class
  * On slides view page run this script on developer console command and slides run automatic
  *
- *  Run this on video - audio slides
- *
+ * Run this on non video - audio slides
  */
-
+var timer = Math.floor(Math.random() * (25000 - 15000 + 1) + 15000);
 var  checkEl = function() {
     console.log("LOAD");
     try{
         // load the element for check
-
-        // time element
-    var textData =  document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("time")[0].innerText.split("/");
-        // slider element
+        // slides element
     var slidesData =  document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("slides")[0].innerText.split("/");
 
     console.log( "slides :",slidesData[0].trim(), slidesData[1].trim());
-    console.log( "time :",textData[0].trim(), textData[1].trim());
     // check the slide
     if(slidesData[0].trim() !== slidesData[1].trim()) {
-        // check the time
-        if (textData[0].trim() === textData[1].trim()) {
             console.log("DONE");
             document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("component_base next")[0].click();
             console.log("CLICK");
-        } else {
-            console.log("WAIT");
-        }
         setTimeout(function () {
             //repeat check
             checkEl();
-        }, 4000);
+        }, timer);
     }else{
         console.log("slides DONE ");
         console.log("END");
