4878ce62db1febeb7ee2631de2ec169a3da0da44
sotiris <sotospez@gmail.com>
2020-04-17 17:05:30 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/**
+ * Auto-play Script for slides viewer on elearningclass.eu class
+ * On slides view page run this script on developer console command and slides run automatic
+ *
+ * Find the slides and time element compere the values and time end then go-to next slide
+ *
+ */
+
+var  checkEl = function() {
+    console.log("LOAD");
+    try{
+        // load the element for check
+        // slider element
+    var textData =  document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("time")[0].innerText.split("/");
+        // lime element
+    var slidesData =  document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("slides")[0].innerText.split("/");
+
+    console.log( "slides :",slidesData[0].trim(), slidesData[1].trim());
+    console.log( "time :",textData[0].trim(), textData[1].trim());
+    // check the slide
+    if(slidesData[0].trim() !== slidesData[1].trim()) {
+        // check the time
+        if (textData[0].trim() === textData[1].trim()) {
+            console.log("DONE");
+            document.getElementById("ContentFrame").contentWindow.document.getElementsByClassName("component_base next")[0].click();
+            console.log("CLICK");
+        } else {
+            console.log("WAIT");
+        }
+        setTimeout(function () {
+            //repeat check
+            checkEl();
+        }, 4000);
+    }else{
+        console.log("slides DONE ");
+        console.log("END");
+    }
+    }catch (e) {
+
+        console.log("error",e);
+    }
+
+};
+checkEl();
+
